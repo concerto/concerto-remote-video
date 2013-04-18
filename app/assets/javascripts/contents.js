85cb14388c8111e38f2ed991b841e0c2cc13d559
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-04-18 00:08:48 -0800

    video info lookup work

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -4,6 +4,7 @@
 // not dry, but no middle man
 function attachHandlers() {
   $('input#remote_video_config_video_id').on('blur', getVideoInfo);
+  $('select#remote_video_config_video_vendor').on('change', getVideoInfo);
 
   function getVideoInfo() {
     // need to know which vendor
@@ -11,7 +12,7 @@ function attachHandlers() {
 
     var info = '<p>Video details could not be determined.</p>';
     var vendor = $('select#remote_video_config_video_vendor').val();
-    var video_id = $(this).val();
+    var video_id = $('input#remote_video_config_video_id').val();
     var info_el = $('.remote-video-info');
 
     if (info_el.length != 0) {
@@ -19,21 +20,42 @@ function attachHandlers() {
       if (typeof vendor != 'undefined') {
         // we found the vendor selection, call appropriate api
         if (vendor == 'YouTube') {
-          // todo
+          $(info_el).empty().html('searching...');
+          $.ajax({
+            url: 'http://gdata.youtube.com/feeds/api/videos?q='+ encodeURIComponent(video_id) +'&v=2&max-results=1&format=5&alt=jsonc',
+            dataType: 'jsonp',
+            timeout: 4000,
+            success: function (data) {
+              if (parseInt(data['data']['totalItems']) > 0) {
+                // we got something, repoint data to first item in results
+                data = data['data']['items'];
+                $(info_el).empty().html('<img src="' + data[0].thumbnail.hqDefault + '"/><h4>' + data[0].title + '</h4><i>' + data[0].duration + ' secs</i><br/><p>' + data[0].description + '</p>');
+              } else {
+                $(info_el).empty().html(info);
+              }
+            },
+            error: function (xoptions, textStatus)  {
+              $(info_el).empty().html(info);
+            }
+          });
         } else if (vendor == 'Vimeo') {
-          $.getJSON('http://vimeo.com/api/v2/video/' + encode(video_id) + '.json',
-            function (data) {
+          $(info_el).empty().html('searching...');
+          $.ajax({
+            url: 'http://vimeo.com/api/v2/video/' + encodeURIComponent(video_id) + '.json',
+            dataType: 'jsonp',
+            timeout: 4000,
+            success: function (data) {
               if (data.length > 0) {
                 // we got something
-                // success
-                $(info_el).replaceWith('<h1>' + data[0].title + '</h1><p>' + data[0].description + '</p>');
+                $(info_el).empty().html('<img src="' + data[0].thumbnail_small + '"/><h4>' + data[0].title + '</h4><i>' + data[0].duration + ' secs</i><br/><p>' + data[0].description + '</p>');
               } else {
-                $(info_el).replaceWith(info);
+                $(info_el).empty().html(info);
               }
-            }).fail(function() {
-              // failure
-              $(info_el).replaceWith(info);
-            });
+            },
+            error: function (xoptions, textStatus)  {
+              $(info_el).empty().html(info);
+            }
+          });
         }
       }
     }
