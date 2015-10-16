756d544f0954898cf104e591bddbaa05be646dd8
Marvin Frederickson <marvin.frederickson@gmail.com>
2015-10-16 11:12:09 -0800

    store api keys in config and use them for videoinfo

--- lib/concerto_remote_video/engine.rb
+++ lib/concerto_remote_video/engine.rb
@@ -11,6 +11,29 @@ module ConcertoRemoteVideo
       @plugin_info ||= plugin_info_class.new do
         add_route("concerto-remote-video", ConcertoRemoteVideo::Engine)
         add_view_hook "frontend/ScreensController", :concerto_frontend_plugins, partial: "frontend/concerto_remote_video.html"
+
+        # Initialize configuration settings with a description and a default.
+        # Administrators can change the value through the Concerto dashboard.
+        add_config("vimeo_api_key", "", 
+                   :value_type => "string",
+                   :category => "API Keys",
+                   :seq_no => 999,
+                   :description => "Vimeo API Access Token.  This token is used for obtaining information about videos when adding video content. http://developer.vimeo.com/apps")
+
+        add_config("youtube_api_key", "", 
+                   :value_type => "string",
+                   :category => "API Keys",
+                   :seq_no => 999,
+                   :description => "YouTube API Access Token.  This token is used for obtaining information about videos when adding video content. http://console.developers.google.com")
+
+        # Some code to run at app boot
+        init do
+          require 'video_info'
+          keys = {}
+          keys[:vimeo] = ConcertoConfig["vimeo_api_key"] unless ConcertoConfig["vimeo_api_key"].blank?
+          keys[:youtube] = ConcertoConfig["youtube_api_key"] unless ConcertoConfig["youtube_api_key"].blank?
+          VideoInfo.provider_api_keys = keys
+        end
       end
     end
   end
